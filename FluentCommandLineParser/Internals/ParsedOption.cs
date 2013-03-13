190148af7d83d978db0bccddfa8b5d541d0249f6
Siy Williams <siy.williams@gmail.com>
2013-03-13 22:30:18 +1300

    refactor the engine slightly with some improvements in mind

--- FluentCommandLineParser/Internals/ParsedOption.cs
+++ FluentCommandLineParser/Internals/ParsedOption.cs
@@ -1,4 +1,28 @@
-﻿namespace Fclp.Internals
+﻿#region License
+// ParsedOption.cs
+// Copyright (c) 2013, Simon Williams
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provide
+// d that the following conditions are met:
+// 
+// Redistributions of source code must retain the above copyright notice, this list of conditions and the
+// following disclaimer.
+// 
+// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+// the following disclaimer in the documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+namespace Fclp.Internals
 {
     /// <summary>
     /// Contains information about a single parsed option and any value.
@@ -32,6 +56,14 @@
         /// Gets or sets any value matched with the key.
         /// </summary>
         public string Value { get; set; }
+
+        /// <summary>
+        /// Gets whether this parsed option has a value set.
+        /// </summary>
+        public bool HasValue
+        {
+            get { return string.IsNullOrEmpty(Value) == false; }
+        }
 
         /// <summary>
         /// Determines whether two specified <see cref="ParsedOption"/> objects have the same values.
