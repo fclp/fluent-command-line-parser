83b12405d2c0abb1b7d800e79643f89fa338a07d
siywilliams <siy.williams@gmail.com>
2013-07-01 22:23:10 +1200

    Seems that serializable usage is not compatible with the PCL framework

--- FluentCommandLineParser/OptionSyntaxException.cs
+++ FluentCommandLineParser/OptionSyntaxException.cs
@@ -29,7 +29,6 @@ namespace Fclp
 	/// <summary>
 	/// Represents an error that has occurred because a Option syntax was in an unexpected format.
 	/// </summary>
-	[Serializable]
 	public class OptionSyntaxException : Exception
 	{
 	}
--- FluentCommandLineParser/UnsupportedTypeException.cs
+++ FluentCommandLineParser/UnsupportedTypeException.cs
@@ -29,7 +29,6 @@ namespace Fclp
 	/// <summary>
 	/// Represents an error that has occurred because a specified type is unsupported.
 	/// </summary>
-	[Serializable]
 	public class UnsupportedTypeException : Exception
 	{
 	}
