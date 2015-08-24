011d25e9545b7fd8cb43335bb6edae9703bc9c62
Richard Carp <mail@rdcarp.com>
2015-08-24 01:13:34 +0100

    Trim surrounding quotes of value when parsing to DateTime.

--- FluentCommandLineParser/Internals/Parsing/OptionParsers/DateTimeCommandLineOptionParser.cs
+++ FluentCommandLineParser/Internals/Parsing/OptionParsers/DateTimeCommandLineOptionParser.cs
@@ -39,7 +39,7 @@ namespace Fclp.Internals.Parsing.OptionParsers
 		/// <returns></returns>
 		public DateTime Parse(ParsedOption parsedOption)
 		{
-			return DateTime.Parse(parsedOption.Value, CultureInfo.CurrentCulture);
+			return DateTime.Parse(parsedOption.Value.Trim('"'), CultureInfo.CurrentCulture);
 		}
 
 		/// <summary>
@@ -50,7 +50,7 @@ namespace Fclp.Internals.Parsing.OptionParsers
 		public bool CanParse(ParsedOption parsedOption)
 		{
 			DateTime dtOut;
-			return DateTime.TryParse(parsedOption.Value, out dtOut);
+			return DateTime.TryParse(parsedOption.Value.Trim('"'), out dtOut);
 		}
 	}
 }
