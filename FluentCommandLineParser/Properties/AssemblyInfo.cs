23b1b593e8791983691ae2bf058683d3854837c7
Alex <sasha.popitich@gmail.com>
2015-09-04 19:03:47 +0300

    Provide a strong named assemblies.

--- FluentCommandLineParser/Properties/AssemblyInfo.cs
+++ FluentCommandLineParser/Properties/AssemblyInfo.cs
@@ -48,8 +48,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b563a988-ae88-4bcc-b6ab-e167f941a167")]
 
 // Allows us to unit test the 'internals'
-//[assembly: InternalsVisibleTo("FluentCommandLineParser.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ad2e88658ff62cac7d7ececd3ac22c6d39ee4e19a09d61acae936f9497df38fa3db020d2b607c8176cd754c7e3a8cdc10559bedcbaaeed76e277f0d009b39bab687261567a1f2da2c3d63b913822ee944664e29bcb85d6b49b87c7d6ee44647ec5252379ed5e4c09d787f6753cf2fdf4a1c1890eedc655738d466bb6f3b91396")]
-[assembly: InternalsVisibleTo("FluentCommandLineParser.Tests")]
+[assembly: InternalsVisibleTo("FluentCommandLineParser.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ad2e88658ff62cac7d7ececd3ac22c6d39ee4e19a09d61acae936f9497df38fa3db020d2b607c8176cd754c7e3a8cdc10559bedcbaaeed76e277f0d009b39bab687261567a1f2da2c3d63b913822ee944664e29bcb85d6b49b87c7d6ee44647ec5252379ed5e4c09d787f6753cf2fdf4a1c1890eedc655738d466bb6f3b91396")]
+//[assembly: InternalsVisibleTo("FluentCommandLineParser.Tests")]
 // !! DO NOT CHANGE - VERSIONS ARE HANDLED AUTOMATICALLY FROM THE CONTINUOUS INTEGRATION SERVER!!
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
